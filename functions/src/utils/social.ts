6d8527654e5835636d38178529b08e29407667c0
Aurélien Loyer <aur.loy@gmail.com>
2019-10-22 11:46:42 +0200

    fix: remeove empty blocs

--- functions/src/utils/social.ts
+++ functions/src/utils/social.ts
@@ -1,4 +1,12 @@
-export function linkedIn(message: string, image: string) {}
-export function facebook(message: string, image: string) {}
-export function twitter(message: string, image: string) {}
-export function whatsapp(message: string) {}
+export function linkedIn(message: string, image: string) {
+    console.log('🗿 LinkedIn');
+}
+export function facebook(message: string, image: string) {
+    console.log('🔭 Facebook');
+}
+export function twitter(message: string, image: string) {
+    console.log('🐦 Twitter');
+}
+export function whatsapp(message: string) {
+    console.log('☎ WhatsApp');
+}
