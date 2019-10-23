32dc1f8d73f8f02ec7ae420c2a77346a797ca76b
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-10-23 14:13:12 +0200

    fix: use latest syntax for lazy loaded module

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import {
-  AngularFireAuthGuard,
-  AngularFireAuthGuardModule,
-  redirectUnauthorizedTo,
-  canActivate
-} from '@angular/fire/auth-guard';
+import { AngularFireAuthGuard, AngularFireAuthGuardModule, redirectUnauthorizedTo, canActivate } from '@angular/fire/auth-guard';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
 
 const routes: Routes = [
-  { path: '', loadChildren: 'src/app/partner/partner.module#PartnerModule' },
-  {
-    path: 'admin',
-    loadChildren: 'src/app/admin/admin.module#AdminModule',
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectUnauthorizedToLogin }
-  }
+    { path: '', loadChildren: () => import('./partner/partner.module').then(module => module.PartnerModule) },
+    {
+        path: 'admin',
+        loadChildren: () => import('./admin/admin.module').then(module => module.AdminModule),
+        canActivate: [AngularFireAuthGuard],
+        data: { authGuardPipe: redirectUnauthorizedToLogin }
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), AngularFireAuthGuardModule],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes), AngularFireAuthGuardModule],
+    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {}
