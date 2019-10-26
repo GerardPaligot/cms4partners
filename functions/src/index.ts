26168cff79803498e7aab5d03d971328bbc5f13e
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-10-26 17:59:48 +0200

    fix: change the from address for email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,6 @@
-import * as functions from "firebase-functions";
-
-export const helloWorldMiddleware = (
-  request: Partial<functions.Request>,
-  response: Pick<functions.Response, "send">
-) => {
-  response.send("Hello from Firebase!");
+import * as functions from 'firebase-functions';
+import { sendEmail } from './utils/mail';
+export const helloWorldMiddleware = (request: Partial<functions.Request>, response: Pick<functions.Response, 'send'>) => {
+    response.send('Hello from Firebase!');
 };
 export const helloWorld = functions.https.onRequest(helloWorldMiddleware);
