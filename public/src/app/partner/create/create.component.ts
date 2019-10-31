c61744408d9e25d2f95cff73089b55ca842f8e58
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-10-31 11:47:57 +0100

    fix: redirect the user to its own page after the creation

--- public/src/app/partner/create/create.component.ts
+++ public/src/app/partner/create/create.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Company, PartnerService } from 'src/app/common/partner.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+    selector: 'app-create',
+    templateUrl: './create.component.html',
+    styleUrls: ['./create.component.scss']
 })
-export class CreateComponent implements OnInit {
-  success = false;
-  constructor(private partnerService: PartnerService) {}
+export class CreateComponent {
+    success = false;
+    constructor(private partnerService: PartnerService, private router: Router) {}
 
-  ngOnInit() {}
-
-  onSubmit(company: Company) {
-    this.partnerService.add(company).then(() => {
-      this.success = true;
-    });
-  }
+    onSubmit(company: Company) {
+        this.partnerService.add(company).then(data => {
+            this.router.navigate(['/dashboard', data.id]);
+        });
+    }
 }
