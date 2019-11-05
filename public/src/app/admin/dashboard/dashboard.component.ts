a6b3e87a41e05e3a72388b17e518846bac8eb792
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-11-05 09:42:54 +0100

    fix: solve runtime error

--- public/src/app/admin/dashboard/dashboard.component.ts
+++ public/src/app/admin/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PartnerService, Company } from 'src/app/common/partner.service';
-import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-dashboard',
@@ -9,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class DashboardComponent implements OnInit {
     partners: Company[];
-    summary: { esnOK: number; esnKO: number; otherOK: number; otherKO: number; total: number };
+    summary: { esnOK?: number; esnKO?: number; otherOK?: number; otherKO?: number; total?: number } = {};
     constructor(private partnerService: PartnerService) {
         this.partnerService.getAll().subscribe(partners => {
             this.partners = partners;
